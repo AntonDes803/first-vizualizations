4361cba41df509e41b47d099ec4acf73dcab735a
Maksimchik36 <Peltek1985@gmail.com>
2025-04-12 21:05:39 +0300

    created sprite, added logo, checked all icons

--- src/sections/homeSections/homePortfolioSection/HomePortfolioSection.jsx
+++ src/sections/homeSections/homePortfolioSection/HomePortfolioSection.jsx
@@ -3,7 +3,17 @@ import styles from "./HomePortfolioSection.module.scss";
 const HomePortfolioSection = ({ lang, dictionary }) => {
   return (
     <section className="section">
-      <div className="container">HomePortfolioSection</div>
+      <div className="container">
+        HomePortfolioSection
+        {/* <div className={styles.iconWrapper}>
+          <svg className={styles.icon}>
+            <use href={`/sprite.svg#icon-logo`} />
+          </svg>
+        </div> */}
+        {/* <div className={styles.iconWrapper}>
+          <img src={`/svg/logo.svg`} alt="logo" />
+        </div> */}
+      </div>
     </section>
   );
 };
