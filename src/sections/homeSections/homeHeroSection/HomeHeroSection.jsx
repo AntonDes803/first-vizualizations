0f91f23cedd58b7fa9e3edc9a7035b8e421d9682
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-04-15 00:26:14 +0300

    start homeHero

--- src/sections/homeSections/homeHeroSection/HomeHeroSection.jsx
+++ src/sections/homeSections/homeHeroSection/HomeHeroSection.jsx
@@ -1,12 +1,32 @@
-import { homeHeroSliderData } from "@/data/homeHeroSliderData";
-import styles from "./HomeHeroSection.module.scss";
+'use client';
+
+import { useEffect, useState } from 'react';
+import { homeHeroSliderData } from '@/data/homeHeroSliderData';
+import styles from './HomeHeroSection.module.scss';
 
 const HomeHeroSection = ({ lang, dictionary }) => {
-  // console.log("homeHeroSliderData", homeHeroSliderData);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % homeHeroSliderData.length);
+    }, 7000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const currentSlide = homeHeroSliderData[currentIndex];
 
   return (
-    <section className={styles.section}>
-      <div className="container">HomeHeroSection</div>
+    <section
+      key={currentIndex}
+      className={`${styles.section} ${styles.slide} ${
+        currentSlide.index === currentIndex ? styles.active : ''
+      }`}
+    >
+      <div className={`container ${styles.container}`}>
+        <h1 className={styles.title}>{currentSlide.titleUk}</h1>
+      </div>
     </section>
   );
 };
