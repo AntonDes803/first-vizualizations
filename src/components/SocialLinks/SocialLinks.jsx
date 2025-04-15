cd8ad3970238813d2c2e7f104334c5a7e75ad677
Maksimchik36 <Peltek1985@gmail.com>
2025-04-15 23:30:23 +0300

    changed sprite, fixed socialLinksComponent, added it to burgerMenu

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,19 +1,11 @@
-import { socLinks } from '@/data/contactsData';
-import styles from './SocialLinks.module.scss';
+import { socLinks } from "@/data/contactsData";
+import styles from "./SocialLinks.module.scss";
 
-const SocialLinks = ({ id, light, dark }) => {
+const SocialLinks = ({ id, dark }) => {
   return (
     <ul id={id} className={styles.socialList}>
       {socLinks.map((item) => {
-        const isInstagram = item.name === 'instagram';
-        const iconClass = [
-          light && styles.socialIconLight,
-          dark && styles.socialIconDark,
-          isInstagram && light && styles.instagramIconLight,
-          isInstagram && dark && styles.instagramIconDark,
-        ]
-          .filter(Boolean)
-          .join(' ');
+        const darkSvgClassname = `${item.name}Dark`;
 
         return (
           <li key={item.name}>
@@ -23,7 +15,13 @@ const SocialLinks = ({ id, light, dark }) => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <svg className={iconClass}>
+              <svg
+                className={
+                  dark
+                    ? `${styles.iconClass} ${styles[darkSvgClassname]}`
+                    : `${styles.iconClass} ${styles[item.name]}`
+                }
+              >
                 <use href={item.icon} />
               </svg>
             </a>
@@ -35,3 +33,43 @@ const SocialLinks = ({ id, light, dark }) => {
 };
 
 export default SocialLinks;
+
+// Ivika
+
+// import { socLinks } from '@/data/contactsData';
+// import styles from './SocialLinks.module.scss';
+
+// const SocialLinks = ({ id, light, dark }) => {
+//   return (
+//     <ul id={id} className={styles.socialList}>
+//       {socLinks.map((item) => {
+//         const isInstagram = item.name === 'instagram';
+//         const iconClass = [
+//           light && styles.socialIconLight,
+//           dark && styles.socialIconDark,
+//           isInstagram && light && styles.instagramIconLight,
+//           isInstagram && dark && styles.instagramIconDark,
+//         ]
+//           .filter(Boolean)
+//           .join(' ');
+
+//         return (
+//           <li key={item.name}>
+//             <a
+//               href={item.href}
+//               className={styles.socLinkItem}
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               <svg className={iconClass}>
+//                 <use href={item.icon} />
+//               </svg>
+//             </a>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// };
+
+// export default SocialLinks;
--- src/sections/homeSections/homePortfolioSection/HomePortfolioSection.jsx
+++ src/sections/homeSections/homePortfolioSection/HomePortfolioSection.jsx
@@ -7,15 +7,6 @@ const HomePortfolioSection = ({ lang, dictionary }) => {
   return (
     <section className="section">
       <div className="container">
-        {/* HomePortfolioSection */}
-        {/* <div className={styles.iconWrapper}>
-          <svg className={styles.icon}>
-            <use href={`/sprite.svg#icon-logo`} />
-          </svg>
-        </div> */}
-        {/* <div className={styles.iconWrapper}>
-          <img src={`/svg/logo.svg`} alt="logo" />
-        </div> */}
         <h2>{dictionary.title}</h2>
       </div>
     </section>
