99164b110f0cae7a1e6fa3dc510c7b80646e9c27
volodymirkavun1 <volodymirkavun1@gmail.com>
2025-04-18 11:12:53 +0300

    add: new project svitlo park first + images

---
+++ src/data/projects/svitloParkFirst.js
@@ -0,0 +1,53 @@
+export const svitloParkFirst = {
+    slug: "",
+    categoryUk: "", // Квартири Будинки Комерція Архітектура
+    categoryEn: "",
+    mainImage: "", // она же идет на портфолиоАйдиХеро
+    altUk: "",
+    altEn: "",
+    typeUk: "", // Квартира Будинок Комерція Архітектура
+    typeEn: "",
+    locationUk: "", // Центральний ЖК “Premium”
+    locationEn: "",
+    area: "", // 49
+    styleUk: "", // сучасний мінімалізм
+    styleEn: "",
+    year: "", // 2024
+  
+    // all arguments before this are for portfolioPage, and all after - will be for idPage
+    videoLink: "",
+    solutions: [
+      {
+        type: "floorPlan",
+        titleUk: "",
+        titleEn: "",
+        textsUk: ["", ""],
+        textsEn: ["", ""],
+        images: [
+          { src: "", altUk: "", altEn: "" },
+          { src: "", altUk: "", altEn: "" },
+        ],
+      },
+      {
+        type: "design",
+        titleUk: "",
+        titleEn: "",
+        textsUk: ["", ""],
+        textsEn: ["", ""],
+        images: [{ src: "", altUk: "", altEn: "" }],
+      },
+      {
+        type: "visualization",
+        titleUk: "",
+        titleEn: "",
+        textsUk: ["", ""],
+        textsEn: ["", ""],
+        images: [
+          { src: "", altUk: "", altEn: "" },
+          { src: "", altUk: "", altEn: "" },
+          { src: "", altUk: "", altEn: "" },
+        ],
+      },
+    ],
+  };
+  
