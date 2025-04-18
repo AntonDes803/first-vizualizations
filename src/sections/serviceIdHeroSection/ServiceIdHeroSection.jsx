32e505e2c9b3f0bba86894b70b147f533cf8a9b5
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-04-18 22:59:07 +0300

    add ServiceIdHeroSection

--- src/sections/serviceIdHeroSection/ServiceIdHeroSection.jsx
+++ src/sections/serviceIdHeroSection/ServiceIdHeroSection.jsx
@@ -1,9 +1,44 @@
-import styles from "./ServiceIdHeroSection.module.scss";
+'use client';
+
+import { useParams } from 'next/navigation';
+import { servicesData } from '@/data/servicesData';
+import HeroBtnArrow from '@/components/HeroBtnArrow/HeroBtnArrow';
+import styles from './ServiceIdHeroSection.module.scss';
+import { stringSplittingByDelimiter } from '@/helpers/stringSplittingByDelimiter';
 
 const ServiceIdHeroSection = ({ lang, dictionary }) => {
+  const params = useParams();
+  const slug = params.slug;
+  const service = servicesData.find((item) => item.slug === `/${slug}`);
+
+  // console.log(service);
+
+  const sectionTitle = service.categoryUk.toUpperCase();
+  const titleArray = stringSplittingByDelimiter(sectionTitle, '’');
+
   return (
-    <section className={styles.section}>
-      <div className="container">ServiceIdHeroSection</div>
+    <section
+      className={styles.section}
+      style={{
+        backgroundImage: `linear-gradient(rgba(45, 48, 55, 0.45), rgba(45, 48, 55, 0.45)), url(${service.images[0]})`,
+      }}
+    >
+      <div className={`container ${styles.container}`}>
+        <div>
+          <h1 className={styles.title}>
+            {titleArray[0]}
+            <span className="ampersandColor">{titleArray[1]}</span>
+            {titleArray[2]}
+          </h1>
+          <div className={styles.descContainer}>
+            <p className={styles.desc}>{dictionary.quote}</p>
+            <p className={`${styles.desc} ${styles.author} `}>
+              {dictionary.quoteAuthor}
+            </p>
+          </div>
+        </div>
+        <HeroBtnArrow href="serviceIdSection" />
+      </div>
     </section>
   );
 };
