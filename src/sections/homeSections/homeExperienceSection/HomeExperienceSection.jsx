93a20923c7f0e5543e60ac4a135a813ff4d02d83
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2025-04-18 21:06:21 +0300

    HomeExperienceSection

--- src/sections/homeSections/homeExperienceSection/HomeExperienceSection.jsx
+++ src/sections/homeSections/homeExperienceSection/HomeExperienceSection.jsx
@@ -1,11 +1,27 @@
-import styles from "./HomeExperienceSection.module.scss";
+import Image from "next/image";
+import s from "./HomeExperienceSection.module.scss";
+import CallBtn from "@/components/buttons/CallBtn/CallBtn";
 
 const HomeExperienceSection = ({ lang, dictionary }) => {
   return (
     <section className="section">
-      <div className="container">
+      <div className={`"container"  ${s.containerHomeExperience}`}>
         {/* HomeExperienceSection */}
-        <h2>{dictionary.title}</h2>
+        <div className={s.boxTitle}>
+          <h2 className={s.title}>{dictionary.title}</h2>
+          <p className={s.text}>{dictionary.description}</p>
+
+          <CallBtn customClass="" text={dictionary.orderBtnText} />
+        </div>
+        <div className={s.boxHomeExperience}>
+          <Image
+            className={s.imgHomeExperience}
+            src="/images/dev/homePage/home-page-work-experience-img.webp"
+            alt="home-page-work-experience-img"
+            sizes="(max-width: 768px ) 100vw, "
+            fill={true}
+          />
+        </div>
       </div>
     </section>
   );
