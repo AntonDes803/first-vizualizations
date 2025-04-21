39ed9aa152e923031d658f1ade4d191652e3429f
volodymirkavun1 <volodymirkavun1@gmail.com>
2025-04-21 17:47:13 +0300

    filling poject kids bedroom

--- src/data/projects/katarData.js
+++ src/data/projects/katarData.js
@@ -2,7 +2,7 @@ export const katarData = {
   slug: "design-katar",
   categoryUk: "Будинки", // Квартири Будинки Комерція Архітектура
   categoryEn: "Houses",
-  mainImage: "/public/images/projects/katar/hall/design-katar-hall-03.webp", // она же идет на портфолиоАйдиХеро
+  mainImage: "/images/projects/katar/hall/design-katar-hall-03.webp", // она же идет на портфолиоАйдиХеро
   altUk: "Візуалізація будинку",
   altEn: "Visualization of the house",
   typeUk: "Будинок", // Квартира Будинок Комерція Архітектура
@@ -50,132 +50,132 @@ export const katarData = {
       ],
       images: [
         { 
-          src: "/public/images/projects/katar/hall/design-katar-hall-01.webp", 
+          src: "/images/projects/katar/hall/design-katar-hall-01.webp", 
           altUk: "Дизайн елегантного холу сучасного будинку", 
           altEn: "Design of an elegant hall of a modern house", 
         },
         { 
-          src: "/public/images/projects/katar/hall/design-katar-hall-02.webp", 
+          src: "/images/projects/katar/hall/design-katar-hall-02.webp", 
           altUk: "Дизайн коридору в елегантному стилі", 
           altEn: "Corridor design in elegant style", 
         },
         { 
-          src: "/public/images/projects/katar/hall/design-katar-hall-04.webp", 
+          src: "/images/projects/katar/hall/design-katar-hall-04.webp", 
           altUk: "Дизайн коридору в золотисто-блакитному стилі", 
           altEn: "Corridor design in golden and blue style", 
         },
         { 
-          src: "/public/images/projects/katar/closet/design-katar-closet-01.webp", 
+          src: "/images/projects/katar/closet/design-katar-closet-01.webp", 
           altUk: "Дизайн елегантної гардеробної", 
           altEn: "Design of elegant dressing room", 
         },
         { 
-          src: "/public/images/projects/katar/closet/design-katar-closet-02.webp", 
+          src: "/images/projects/katar/closet/design-katar-closet-02.webp", 
           altUk: "Візуалізація макіяжного столу в сучасному будинку", 
           altEn: "Visualization of a makeup table in a modern house", 
         },
         { 
-          src: "/public/images/projects/katar/closet/design-katar-closet-03.webp", 
+          src: "/images/projects/katar/closet/design-katar-closet-03.webp", 
           altUk: "Скляна гардеробна в елегантному будинку", 
           altEn: "Glass dressing room in an elegant house", 
         },
         { 
-          src: "/public/images/projects/katar/closet/design-katar-closet-04.webp", 
+          src: "/images/projects/katar/closet/design-katar-closet-04.webp", 
           altUk: "Дизайн гардеробної в сучасному стилі з острівком", 
           altEn: "Modern dressing room design with an island", 
         },
         { 
-          src: "/public/images/projects/katar/living-room/design-katar-living-room-01.webp", 
+          src: "/images/projects/katar/living-room/design-katar-living-room-01.webp", 
           altUk: "Дизайн вітальні в сучасному будинку з великим настінним телевізором", 
           altEn: "Living room design in a modern home with a large wall-mounted TV", 
         },
         { 
-          src: "/public/images/projects/katar/living-room/design-katar-living-room-02.webp", 
+          src: "/images/projects/katar/living-room/design-katar-living-room-02.webp", 
           altUk: "Дизайн вітальні в світлих тонах", 
           altEn: "Living room design in light colors", 
         },
         { 
-          src: "/public/images/projects/katar/living-room/design-katar-living-room-03.webp", 
+          src: "/images/projects/katar/living-room/design-katar-living-room-03.webp", 
           altUk: "Сучасна вітальня з великим каміном та зеленим диваном", 
           altEn: "Modern living room with large fireplace and green sofa", 
         },
         { 
-          src: "/public/images/projects/katar/living-room/design-katar-living-room-04.webp", 
+          src: "/images/projects/katar/living-room/design-katar-living-room-04.webp", 
           altUk: "Вітальня в світло-зелений тонах із телевізором", 
           altEn: "Living room in light green tones with TV", 
         },
         { 
-          src: "/public/images/projects/katar/living-room/design-katar-living-room-05.webp", 
+          src: "/images/projects/katar/living-room/design-katar-living-room-05.webp", 
           altUk: "Сучасна вітальня із зеленими диванами та столом по центрі", 
           altEn: "Modern living room with green sofas and a table in the center", 
         },
         { 
-          src: "/public/images/projects/katar/living-room/design-katar-office-place-01.webp", 
+          src: "/images/projects/katar/living-room/design-katar-office-place-01.webp", 
           altUk: "Дизайн кабінету в сучасному елегантному будинку", 
           altEn: "Study design in a modern elegant home", 
         },
         { 
-          src: "/public/images/projects/katar/bedroom/design-katar-bedroom-01.webp", 
+          src: "/images/projects/katar/bedroom/design-katar-bedroom-01.webp", 
           altUk: "Візуалізація світло-рожевої спальні із великим ліжком", 
           altEn: "Visualization of a light pink bedroom with a large bed", 
         },
         { 
-          src: "/public/images/projects/katar/bedroom/design-katar-bedroom-02.webp", 
+          src: "/images/projects/katar/bedroom/design-katar-bedroom-02.webp", 
           altUk: "Візуалізація світло-рожевої спальні із великим настінним телевізором", 
           altEn: "Visualization of a light pink bedroom with a large wall-mounted TV", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-01.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-01.webp", 
           altUk: "Дизайн ванної кімнати в світло-рожевих тонах та настінним дзеркалом та LED-підсвіткою", 
           altEn: "Bathroom design in light pink tones with wall mirror and LED lighting", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-02.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-02.webp", 
           altUk: "Вання кімната з настінним дзеркалом та шафою із LED-підсвіткою", 
           altEn: "Bathroom with wall mirror and cabinet with LED lighting", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-03.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-03.webp", 
           altUk: "Візуалізація ванної кімнати із скляним прозорим відгородженням", 
           altEn: "Visualization of a bathroom with a transparent glass partition", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-04.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-04.webp", 
           altUk: "Візуалізація душової кабіни із полицями та LED-підсвіткою", 
           altEn: "Visualization of a shower enclosure with shelves and LED lighting", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-05.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-05.webp", 
           altUk: "Візуалізація дизайну ванної кімнати з настінним дзеркалом та шафою із LED-підсвіткою", 
           altEn: "Visualization of the bathroom design with a wall mirror and a cabinet with LED lighting", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-06.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-06.webp", 
           altUk: "Візуалізація дизайну ванної кімнати з настінним дзеркалом із LED-підсвіткою", 
           altEn: "Visualization of the bathroom design with a wall mirror with LED lighting", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-07.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-07.webp", 
           altUk: "Візуалізація душової кабіни із полицями та LED-підсвіткою на фоні світлої мармурової стіни", 
           altEn: "Visualization of a shower enclosure with shelves and LED lighting against a light marble wall", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-08.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-08.webp", 
           altUk: "Візуалізація умивальника та настінного дзеркала із LED-підсвіткою", 
           altEn: "Visualization of a washbasin and wall mirror with LED lighting", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-09.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-09.webp", 
           altUk: "Візуалізація дизайну ванної кімнати з настінним дзеркалом із LED-підсвіткою та світлої мармурової стіни", 
           altEn: "Visualization of the bathroom design with a wall mirror with LED lighting and a light marble wall", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-10.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-10.webp", 
           altUk: "Візуалізація дизайну ванної кімнати з настінним дзеркалом та шафою із LED-підсвіткою та світлої мармурової стіни", 
           altEn: "Bathroom design visualization with wall mirror and cabinet with LED lighting and light marble wall", 
         },
         { 
-          src: "/public/images/projects/katar/bathroom/design-katar-bathroom-11.webp", 
+          src: "/images/projects/katar/bathroom/design-katar-bathroom-11.webp", 
           altUk: "Візуалізація дизайну ванної кімнати з настінним дзеркалом та шафою із LED-підсвіткою зеленої підставки для умивальнику", 
           altEn: "Visualization of the bathroom design with a wall mirror and a cabinet with LED lighting of a green washbasin stand", 
         },
